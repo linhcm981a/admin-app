cef7e4b43d2d0dc84a4d1d6e6e176fbdab57a10c
linh linh <linhcm981a@gmail.com>
2019-11-18 20:06:40 +0700

    fix heroku

---
+++ build/precache-manifest.cdfca972d5a7aa9bb048a3dbfecba2d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e3a3f1682f65d1d6306c64cb990eefa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6adbbc8525e76f354275",
+    "url": "/static/css/main.8b4db4ce.chunk.css"
+  },
+  {
+    "revision": "5823103577764fadd8ad",
+    "url": "/static/js/2.944233e4.chunk.js"
+  },
+  {
+    "revision": "6adbbc8525e76f354275",
+    "url": "/static/js/main.4d62521a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9a76e74e107e6163dc82116b8236c132",
+    "url": "/static/media/login.9a76e74e.svg"
+  }
+]);
